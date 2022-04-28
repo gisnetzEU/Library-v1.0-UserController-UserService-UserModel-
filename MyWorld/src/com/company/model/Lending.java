2183c2380a8b88c405c0e01046f8f79c1deed0cf
Alumne_mati1 <rbeyloc@gmail.com>
2022-04-28 13:57:41 +0200

    First draft of Lending Model

---
+++ MyWorld/src/com/company/model/Lending.java
@@ -0,0 +1,71 @@
+package com.company.model;
+
+import java.time.LocalDate;
+
+public class Lending {
+
+    public enum Status { ONDATE, OUTOFDATE, RETURNED }
+
+    private final User user;
+    private final Ejemplar ejemplar;
+    private final LocalDate lendingDate;
+    private final LocalDate returnLimitDate;
+    private LocalDate returnRealDate;
+    private Status status;
+
+    public Lending(User user, Ejemplar ejemplar) {
+        this.user = user;
+        this.ejemplar = ejemplar;
+        this.lendingDate = LocalDate.now();
+        this.returnLimitDate = this.lendingDate.plusDays(7);
+        this.status = status.ONDATE;
+    }
+
+    public void devolution() {
+        this.returnRealDate = LocalDate.now();
+        this.status = status.RETURNED;
+    }
+
+    public void setOutOfDate() {
+        if(this.returnLimitDate.isBefore(LocalDate.now())) {
+            this.status = status.OUTOFDATE;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return  "\nLending:" +
+                "\n  user=" + user +
+                "\n  ejemplar=" + ejemplar +
+                "\n  lendingDate=" + lendingDate +
+                "\n  returnLimitDate=" + returnLimitDate +
+                "\n  returnRealDate=" + returnRealDate +
+                "\n  status=" + status +
+                "\n";
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Ejemplar getEjemplar() {
+        return ejemplar;
+    }
+
+    public LocalDate getLendingDate() {
+        return lendingDate;
+    }
+
+    public LocalDate getReturnLimitDate() {
+        return returnLimitDate;
+    }
+
+    public LocalDate getReturnRealDate() {
+        return returnRealDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+}
